1c3c681b9f1c4c9bbc984d32994dceb9fc1b6739
manish0183 <221manish0036@dbit.in>
2024-07-16 23:28:57 +0530

    nav&footer

--- nav.js
+++ nav.js
@@ -47,47 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const dropdownButtons = document.querySelectorAll('.dropdown-btn');
-//   let activeDropdown = null;
 
-//   dropdownButtons.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//       const dropdownId = button.getAttribute('data-dropdown');
-//       const dropdown = document.getElementById(dropdownId);
-
-//       if (activeDropdown && activeDropdown !== dropdown) {
-//         activeDropdown.classList.remove('active');
-//       }
-
-//       dropdown.classList.toggle('active');
-//       activeDropdown = dropdown.classList.contains('active') ? dropdown : null;
-//     });
-//   });
-
-  
-//   document.addEventListener('click', (e) => {
-//     if (
-//       !e.target.closest('.dropdown') &&
-//       !e.target.closest('.dropdown-btn')
-//     ) {
-//       if (activeDropdown) {
-//         activeDropdown.classList.remove('active');
-//         activeDropdown = null;
-//       }
-//     }
-//   });
-
-  
-//   const hamburger = document.getElementById('hamburger');
-//   const menu = document.querySelector('.menu');
-
-//   hamburger.addEventListener('click', () => {
-//     const isExpanded = hamburger.getAttribute('aria-expanded') === 'true';
-//     hamburger.setAttribute('aria-expanded', !isExpanded);
-//     menu.style.display = isExpanded ? 'none' : 'flex';
-//   });
-// });
 
 
 
